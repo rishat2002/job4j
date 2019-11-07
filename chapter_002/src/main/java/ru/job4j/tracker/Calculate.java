3779461e24bafced2bb3210db1935c86224579cf
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-11-08 02:10:28 +0300

    2. Чтение кода и базовые ошибки.[#189663]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Calculate.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+/**
+ * Package for calculate task.
+ *
+ * @author Rishat
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Calculate {
+    /**finding amount
+     * @param first
+     * @param second
+     */
+    public static void add(double first, double second) {
+        double result =  first + second;
+        System.out.println(first + "+" + second + " = " + result);
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        add(1, 1);
+    }
+}
