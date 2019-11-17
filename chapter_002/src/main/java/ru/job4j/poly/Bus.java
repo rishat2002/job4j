9bdb5eaeeb4d5461f89e3c459287a5af6923d48c
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-11-17 09:16:58 +0300

    3. Интерфейс Input[#189676]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public int passenger() {
+        return 0;
+    }
+
+    @Override
+    public int fillfuel(int fuelcount) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,8 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+    int  passenger();
+    int fillfuel(int fuelcount);
+
+}
