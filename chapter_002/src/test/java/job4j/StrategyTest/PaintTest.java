9d1260fd8de254e46175aeaa5a1f7266ee38129a
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-11-24 11:59:06 +0300

    9.1 Рефакторинг теста @Before @After[#189667]

--- chapter_002/src/test/java/job4j/StrategyTest/PaintTest.java
+++ chapter_002/src/test/java/job4j/StrategyTest/PaintTest.java
@@ -1,54 +1,63 @@
 package job4j.StrategyTest;
-
 import job4j.stragery.Paint;
 import job4j.stragery.Square;
 import job4j.stragery.Triangle;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
-                        new StringBuilder()
-                                .append("@@@@\n")
-                                .append("@  @\n")
-                                .append("@  @\n")
-                                .append("@@@@\n")
-                                .append(System.lineSeparator())
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
+                                .add("@@@@")
+                                .add("@  @")
+                                .add("@  @")
+                                .add("@@@@")
                                 .toString()
                 )
         );
-
-        System.setOut(stdout);
     }
+
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
-                        new StringBuilder()
-                        .append("  1  \n")
-                                .append(" 111 \n")
-                                .append("11111\n")
-                                .append(System.lineSeparator())
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
+                                .add("  1  ")
+                                .add(" 111 ")
+                                .add("11111")
                                 .toString()
                 )
         );
-
-        System.setOut(stdout);
     }
 }
