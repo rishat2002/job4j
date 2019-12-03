556026ef7c9b7c766e20b736fe791ef847586c53
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-12-04 01:29:21 +0300

    1. Конвертация листа массивов в один лист Integer[#189648]

--- chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,25 +1,37 @@
 package ru.job4j.list;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
-        int cells =list.size()/rows;
-        if (!(list.size()%rows==0)) {
-            cells=list.size()/rows+1;
+        int cells = list.size() / rows;
+        if (!(list.size() % rows == 0)) {
+            cells = list.size() / rows + 1;
         }
-        if (!(cells*rows==list.size())) {
-            for (int i=list.size();i<cells*rows;i++) {
+        if (!(cells * rows == list.size())) {
+            for (int i = list.size(); i < cells * rows; i++) {
                 list.add(0);
             }
         }
         int[][] array = new int[rows][cells];
-        int listnum=0;
-        for (int i=0;i<rows;i++) {
-            for (int j=0;j<cells;j++) {
-                array[i][j]=list.get(listnum);
+        int listnum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cells; j++) {
+                array[i][j] = list.get(listnum);
                 listnum++;
             }
         }
         return array;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> list2 = new ArrayList<Integer>();
+        for (int[] n : list) {
+            for (Integer n1 : n) {
+                list2.add(n1);
+            }
+        }
+        return list2;
     }
 }
