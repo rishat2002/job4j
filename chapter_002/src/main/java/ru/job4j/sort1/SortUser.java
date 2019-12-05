e2415c822d1c89c148e20fdcdb652ddfef714625
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-12-06 01:48:58 +0300

    2. Сортировка User с использованием Comparator[#189635]

--- chapter_002/src/main/java/ru/job4j/sort1/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort1/SortUser.java
@@ -1,31 +1,68 @@
 package ru.job4j.sort1;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-
+import java.util.*;
 public class SortUser {
-    public Set<User> sort (List<User> list) {
+    public Set<User> sortage (List<User> list) {
      Set<User>userset=new TreeSet<>();
      for (User n:list) {
          userset.add(n);
      }
-     for (User m:userset) {
-         System.out.println(m.getName()+" "+m.getAge());
-     }
      return userset;
+    }
+    public List<User> sortNameLength (List<User>list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int n=0;
+               if (o1.getName().length()>o2.getName().length()) {
+                   n=1;
+               }
+               else if(o1.getName().length()<o2.getName().length()) {
+                   n=-1;
+               }
+                return n;
+            }
+        });
+        for (User m:list) {
+            System.out.println(m.getName()+" "+m.getAge());
+        }
+        return list;
+    }
+    public List<User> sortByAllFields (List<User>list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }.thenComparing(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int n=0;
+                if (o1.getAge()>o2.getAge()) {
+                    n=1;
+                }
+                else if(o1.getAge()<o2.getAge()) {
+                    n=-1;
+                }
+                return n;
+            }
+        }));
+        for (User m:list) {
+            System.out.println(m.getName()+" "+m.getAge());
+        }
+        return list;
     }
 
     public static void main(String[] args) {
         SortUser sort1=new SortUser();
-        User user1=new User(12,"pasha");
-        User user2=new User(16,"semen");
-        User user3=new User(14,"slava");
+        User user1=new User(22,"pasha");
+        User user2=new User(16,"pasha");
+        User user3=new User(13,"slava");
+        User user4=new User(14,"slava");
         List<User>lit=new ArrayList<>();
         lit.add(user1);
-        lit.add(user2);
         lit.add(user3);
-        sort1.sort(lit);
+        lit.add(user4);
+        lit.add(user2);
+        sort1.sortByAllFields(lit);
     }
 }
