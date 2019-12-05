2878e1cd1c0cd0bafce9fa5759bdd1def3296b82
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-12-05 15:53:31 +0300

    3. Изменить программу Tracker из 2-го модуля[#189649]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,12 +1,14 @@
 package ru.job4j.tracker;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final List<Item>itemlist= new ArrayList<Item>();
     private int position = 0;
     public Item add(Item item) {
         item.setId(this.generateId());
-        items[this.position++] = item;
+        itemlist.add(item);
         return item;
     }
     private String generateId() {
@@ -15,61 +17,47 @@ public class Tracker {
     }
     public boolean replace(String id, Item item) {
         boolean result=false;
-        for (int i=0;i<position;i++) {
-            if(items[i].getId().equals(id)) {
-                String oldid=items[i].getId();
-                items[i]=item;
-               items[i].setId(oldid);
+        for (int i=0;i<itemlist.size();i++) {
+            if(itemlist.get(i).getId().equals(id)) {
+                String oldid=itemlist.get(i).getId();
+                itemlist.remove(i);
+                itemlist.add(i,item);
                 result=true;
                 break;
             }
         }
         return result;
     }
+    public List<Item> findall() {
+        return itemlist;
+    }
+
     public boolean delete(String id) {
         boolean result=false;
         int n=0;
-        for (int i=0;i<position;i++) {
-            if (items[i].equals(id)) {
-                n=i;
+        for (int i=0;i<itemlist.size();i++) {
+            if (itemlist.get(i).getId().equals(id)) {
+                itemlist.remove(i);
                 result=true;
                 break;
             }
         }
-        items[n]=null;
-        for (int i=n;i<items.length-1;i++) {
-            items[i]=items[i+1];
-        }
-        items[items.length-1]=null;
         return result;
     }
-    public Item[] findAll() {
-     Item []newitems=new Item[position];
-     System.arraycopy(items, 0, newitems, 0, position);
-     return newitems;
-    }
-    public Item[] findByName(String key) {
-        int newitemindex=0;
-        for (int i=0;i<position;i++) {
-            if(key.equals(items[i].getName())) {
-                newitemindex++;
+    public List<Item> findByName(String key) {
+        List<Item>newlist=new ArrayList<Item>();
+        for (int i=0;i<itemlist.size();i++) {
+            if(key.equals(itemlist.get(i).getName())) {
+                newlist.add(itemlist.get(i));
             }
         }
-        Item[] newitem=new Item[newitemindex];
-        newitemindex=0;
-        for (int i=0;i<position;i++) {
-            if(key.equals(items[i].getName())) {
-                newitem[newitemindex]=items[i];
-                newitemindex++;
-            }
-        }
-        return newitem;
+        return newlist;
     }
     public Item findById(String id) {
         Item iditem=null;
-        for (int i=0;i<items.length;i++) {
-            if(id.equals(this.items[i].getId())) {
-            iditem=this.items[i];
+        for (int i=0;i<itemlist.size();i++) {
+            if(id.equals(itemlist.get(i).getId())) {
+            iditem=itemlist.get(i);
             break;
             }
         }
@@ -77,28 +65,5 @@ public class Tracker {
     }
 
     public static void main(String[] args) {
-        Item item1=new Item("vasya");
-        Item item2=new Item("pit");
-        Item item3=new Item("slava");
-        Item item4=new Item("spot");
-        Tracker tracker1=new Tracker();
-        tracker1.add(item1);
-        tracker1.add(item2);
-        tracker1.add(item3);
-        Item []newmas=tracker1.findAll();
-        System.out.println(newmas.length);
-       for (int i=0;i<newmas.length;i++) {
-           System.out.println(newmas[i].getId());
-       }
-        for (int i=0;i<newmas.length;i++) {
-            System.out.println(newmas[i].getName());
-        }
-        tracker1.delete(item2.getId());
-        newmas=tracker1.findAll();
-        System.out.println(newmas.length);
-        for (int i=0;i<newmas.length;i++) {
-            System.out.println(newmas[i].getName());
-        }
-
     }
 }
--- chapter_002/src/test/java/TrackerTest.java
+++ chapter_002/src/test/java/TrackerTest.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
@@ -30,16 +33,13 @@ public class TrackerTest {
     public void  findByNameTest() {
         Tracker tracker = new Tracker();
         Item previous = new Item("test1");
-        Item previous1 = new Item("test1");
+        Item previous1 = new Item("test2");
         tracker.add(previous);
         tracker.add(previous1);
-        Item next = new Item("test2");
-        tracker.add(next);
-        tracker.findByName(next.getName());
-        Item[] resultmas=tracker.findAll();
-        Item[] truemas={previous,previous1};
-        assertArrayEquals(resultmas,truemas);
-
+        List<Item> resul=tracker.findByName("test2");
+        List<Item> n=new ArrayList<Item>();
+        n.add(previous1);
+        assertArrayEquals(resul.toArray(),n.toArray());
     }
     @Test
     public void DeleteTest() {
@@ -48,12 +48,11 @@ public class TrackerTest {
         Item previous1 = new Item("test2");
         tracker.add(previous);
         tracker.add(previous1);
-        Item next = new Item("test3");
-        tracker.add(next);
-        tracker.delete(next.getId());
-        Item[] resultmas=tracker.findAll();
-        Item[] truemas={previous,previous1};
-        assertArrayEquals(resultmas,truemas);
+        tracker.delete(previous1.getId());
+        List<Item> resul=tracker.findall();
+        List<Item> n=new ArrayList<Item>();
+        n.add(previous);
+        assertArrayEquals(resul.toArray(),n.toArray());
 
     }
     @Test
@@ -68,7 +67,4 @@ public class TrackerTest {
         Item item=tracker.findById(next.getId());
         assertThat(item,is(next));
 
-    }
-
-
-}
+    }}
