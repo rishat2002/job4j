c07d4a845e816b31977e01c4f1f323e1772d59d6
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-12-13 04:23:31 +0300

    3. Уникальность элементов и сортировка.[#189524]

--- chapter_002/src/main/java/API/Address.java
+++ chapter_002/src/main/java/API/Address.java
@@ -2,11 +2,20 @@ package API;
 
 public class Address {
     private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     private String street;
     private int home;
     private int apartment;
 
-    public Address(String city, String street, int home, int apartment) {
+    public Address(String city, String street) {
         this.city = city;
         this.street = street;
         this.home = home;
@@ -15,6 +24,17 @@ public class Address {
 
     @Override
     public String toString() {
-        return "Client live "+"city "+city+" street "+street+" home "+home+" apartment "+apartment;
+        return "Client live "+"city "+city+" street "+street;
+    }
+
+    @Override
+    public boolean equals(Object a) {
+        Address n=(Address) a;
+        return this.city.equals(n.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return city.length();
     }
 }
--- chapter_002/src/main/java/API/Profiles.java
+++ chapter_002/src/main/java/API/Profiles.java
@@ -1,17 +1,23 @@
 package API;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         List<Address> adres=profiles.stream().map(prof->prof.getAddress()).collect(Collectors.toList());
+        adres.sort((s1,s2)->{
+            return s1.getCity().compareTo(s2.getCity());
+        });
+        adres=adres.stream().distinct().collect(Collectors.toList());
         return adres;
     }
 
     public static void main(String[] args) {
         Profiles pro=new Profiles();
-        List<Profile> profiles=List.of(new Profile(new Address("tyla","smirnova",13,210)),new Profile(new Address("tyla","smirnova",26,220)));
+        List<Profile> profiles=List.of(new Profile(new Address("ayla","smirnova")),new Profile(new Address("cyla","smirnova")),
+                new Profile(new Address("ayla","s")));
         List<Address> alladress=pro.collect(profiles);
         System.out.println(alladress);
     }
