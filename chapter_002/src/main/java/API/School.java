98e439fb5c06cc8a55389aebf432260049d5b805
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-12-17 03:39:23 +0300

    0. Stream API улучшения[#189528]

--- chapter_002/src/main/java/API/School.java
+++ chapter_002/src/main/java/API/School.java
@@ -1,20 +1,25 @@
 package API;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class School {
      public List<Student> collect(List<Student> students, Predicate<Student> predict) {
          List<Student> sortClass=students.stream().filter(predict).collect(Collectors.toList());
          return sortClass;
      }
-
+    public  List<Student> levelOf(List<Student> students,int bound) {
+         List<Student>newlist=students.stream().flatMap(Stream::ofNullable).sorted((s2,s1)-> Integer.compare(s1.getScore(),s2.getScore())).takeWhile(t->t.getScore()>=bound).collect(Collectors.toList());
+        return newlist;
+    }
     public static void main(String[] args) {
-        Map<String,Student>students=List.of(new Student("Pasha"),new Student("Vlad")).stream().collect(Collectors.toMap(e->e.getSurname(),
-                e->e));
-        System.out.println(students);
+        School scool = new School();
+     List<Student>newlist=List.of(new Student("pasha",50),new Student("vova",32),new Student("semen",90));
+        System.out.println(scool.levelOf(newlist,52));
     }
 
 }
--- chapter_002/src/main/java/API/Student.java
+++ chapter_002/src/main/java/API/Student.java
@@ -1,5 +1,10 @@
 package API;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Student {
     private int score;
     private String surname;
@@ -23,12 +28,24 @@ public class Student {
     public Student(int score) {
         this.score = score;
     }
-    public Student(String surname) {
+    public Student(String surname,int score) {
         this.surname = surname;
+        this.score=score;
     }
 
     @Override
     public String toString() {
         return surname+" ";
     }
+    @Override
+    public boolean equals(Object a) {
+        Student n=(Student) a;
+        return this.getSurname().equals(n.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return score;
+    }
+
 }
