2a7e74693edd896510f606674420a19845ebcb41
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2019-12-31 09:49:41 +0300

    5.2.1. Реализовать SimpleArray<T>[#216748]

---
+++ chapter_002/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,58 @@
+package ru.job4j.generic;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable<T> {
+
+    public Object[] getArray() {
+        return array;
+    }
+
+    private Object[] array;
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.array = new Object[size];
+    }
+
+    public void add(T model) {
+        this.array[index++] = model;
+    }
+
+    public void set(int position, T model) {
+        this.array[position] = model;
+    }
+
+    public void remove(int position) {
+        this.array[position] = null;
+        if (position != this.array.length - 1) {
+            System.arraycopy(this.array, position + 1, this.array, position, this.array.length - position - 1);
+            this.array[this.array.length - 1] = null;
+        }
+    }
+    public T get(int position) {
+        return (T) this.array[position];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int i=0;
+            @Override
+            public boolean hasNext() {
+                if (i==array.length) {
+                    return false;
+                }
+                else return true;
+            }
+
+            @Override
+            public T next() throws NoSuchElementException {
+                if (i==array.length) {
+                    new NoSuchElementException();
+                }
+                return (T) array[i++];
+            }
+        };
+    }
+}
