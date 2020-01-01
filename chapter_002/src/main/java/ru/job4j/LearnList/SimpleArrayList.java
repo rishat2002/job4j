402673df35b040fc3e8ee083b8fe8a0fd3f08d20
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-01-01 08:03:59 +0300

    5.3.2. Создать контейнер на базе связанного списка [#216738]

--- chapter_002/src/main/java/ru/job4j/LearnList/SimpleArrayList.java
+++ chapter_002/src/main/java/ru/job4j/LearnList/SimpleArrayList.java
@@ -1,6 +1,11 @@
 package ru.job4j.LearnList;
 
-public class SimpleArrayList<E> {
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArrayList<E> implements Iterable<E> {
+    private int modCount=0;
     private int size;
     private Node<E> first;
 
@@ -9,13 +14,27 @@ public class SimpleArrayList<E> {
         newLink.next = this.first;
         this.first = newLink;
         this.size++;
+        this.modCount++;
+    }
+    public void addInEnd(E data) {
+        if (size==0) {
+            this.first=new Node<>(data);
+        }
+        else {
+        Node<E> result = this.first;
+        for (int i = 0; i<size-1 ; i++) {
+            result = result.next;
+        }
+        result.next = new Node<>(data);}
+        this.size++;
+        this.modCount++;
     }
 
-    public E delete() {
+    public void delete() {
         Node<E> newLink=this.first.next;
         this.first=newLink;
         size--;
-        return newLink.data;
+        modCount++;
     }
 
     public E get(int index) {
@@ -25,7 +44,6 @@ public class SimpleArrayList<E> {
         }
         return result.data;
     }
-
     public int getSize() {
         return this.size;
     }
@@ -39,4 +57,29 @@ public class SimpleArrayList<E> {
             this.data = data;
         }
     }
+    @Override
+    public Iterator iterator() {
+        return new Iterator<E>() {
+            private int s=modCount;
+            private int i=0;
+            Node<E> result = first;
+            @Override
+            public boolean hasNext() {
+                if (i<size) return true;
+                else return false;
+            }
+            @Override
+            public E next() throws NoSuchElementException, ConcurrentModificationException {
+                if (s!=modCount) throw  new ConcurrentModificationException();
+                if (!this.hasNext()) throw new NoSuchElementException();
+                E obj=result.data;
+                result=result.next;
+                i++;
+                return obj;
+            }
+        };
+    }
+
+
+
 }
--- chapter_002/src/test/java/ru/job4j/LearnList/SimpleArrayListTest.java
+++ chapter_002/src/test/java/ru/job4j/LearnList/SimpleArrayListTest.java
@@ -1,6 +1,11 @@
 package ru.job4j.LearnList;
 import org.junit.Test;
 import org.junit.Before;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 public class SimpleArrayListTest
@@ -30,5 +35,26 @@ public class SimpleArrayListTest
         list.delete();
         assertThat(list.get(0), is(2));
         assertThat(2, is(list.getSize()));
+    }
+    @Test
+    public void iteratorTest() {
+        Iterator newiter=list.iterator();
+        assertThat(list.get(0),is(newiter.next()));
+        assertThat(list.get(1),is(newiter.next()));
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void iterator1Test() {
+        Iterator newiter=list.iterator();
+        newiter.next();
+        newiter.next();
+        newiter.next();
+        newiter.next();
+    }
+    @Test(expected = ConcurrentModificationException.class)
+    public void iterator2Test() {
+        Iterator newiter=list.iterator();
+        newiter.next();
+        list.add(4);
+        newiter.next();
     }
 }
