dbdb10e9b402c4509bc00a334dca8b3e2d6d44aa
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-01-03 08:17:06 +0300

    5.3.1. Создать динамический список на базе массива.[#216737]

--- chapter_002/src/main/java/ru/job4j/LearnList/ArrList.java
+++ chapter_002/src/main/java/ru/job4j/LearnList/ArrList.java
@@ -5,43 +5,58 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ArrList<E> implements Iterable<E> {
-    private Object[]mass;
-    private int index=0;
-    private int modCount=0;
+    private Object[] mass;
+    private int index = 0;
+    private int modCount = 0;
+
     public ArrList() {
         this.mass = new Object[10];
     }
-    public void add(E value){
-    modCount++;
-    if (index<mass.length) {
-        mass[index]=value;
-        index++;
-    }
-    else {
-    Object[] biggermass=new Object[mass.length*2];
-    System.arraycopy(mass,0,biggermass,0,mass.length);
-    mass=biggermass;
-    mass[index++]=value;
-    }}
 
-    public E get(int i){
-    return (E) mass[i];
+    public void add(E value) {
+        modCount++;
+        if (index < mass.length) {
+            mass[index] = value;
+            index++;
+        } else {
+            Object[] biggermass = new Object[mass.length * 2];
+            System.arraycopy(mass, 0, biggermass, 0, mass.length);
+            mass = biggermass;
+            mass[index++] = value;
+        }
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public E get(int i) {
+        return (E) mass[i];
     }
 
     @Override
     public Iterator iterator() {
         return new Iterator<E>() {
-            private int s=modCount;
-            private int i=0;
+            private int s = modCount;
+            private int i = 0;
+
             @Override
             public boolean hasNext() {
-                if (i<=index) return true;
-                else return false;
+                boolean result = false;
+                if (i < index) {
+                    result = true;
+                }
+                return result;
             }
+
             @Override
-            public E next() throws NoSuchElementException,ConcurrentModificationException{
-                if (s!=modCount) throw  new ConcurrentModificationException();
-                if (i>index) throw new NoSuchElementException();
+            public E next() throws NoSuchElementException, ConcurrentModificationException {
+                if (s != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (i > index) {
+                    throw new NoSuchElementException();
+                }
                 return (E) mass[i++];
             }
         };
