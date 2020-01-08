bf5723ad99226031b24d652a5d5edd89c43f5095
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-01-09 00:24:40 +0300

    5.3.3.1 Очередь на двух стеках[#216739]

--- chapter_002/src/main/java/ru/job4j/LearnList/SimpleQueu.java
+++ chapter_002/src/main/java/ru/job4j/LearnList/SimpleQueu.java
@@ -1,9 +1,19 @@
 package ru.job4j.LearnList;
 
-public class SimpleQueu<T> extends SimpleStack<T> {
+public class SimpleQueu<T>  {
+   private SimpleStack<T> stack1=new SimpleStack<>();
+   private SimpleStack<T> stack2=new SimpleStack<>();
+   private int i=0;
 
-    public void push(T value){
-        this.getList().addInEnd(value);
+    public void push(T value) {
+        stack1.push(value);
+        i++;
+    }
+    public T poll() {
+        for (int n=0;n<i;n++) {
+            stack2.push(stack1.poll());
+        }
+        return stack2.poll();
     }
 
 }
