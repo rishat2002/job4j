a8402a15e9e770251ca3dce73a092bc989900213
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-01-15 02:35:55 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#216740]

--- chapter_002/src/main/java/ru/job4j/LearnList/Node.java
+++ chapter_002/src/main/java/ru/job4j/LearnList/Node.java
@@ -9,34 +9,20 @@ public class Node<T> {
         this.value = value;
     }
 
-    boolean hasCycle1(Node<T> node) {
-        boolean valid = false;
-        String loop = "";
-        Node<T> temp = node;
-        while (temp.next != null) {
-            loop += temp.toString();
-            temp = temp.next;
-            if (loop.contains(temp.toString())) {
-                valid = true;
-                break;
-            }
-        }
-        return valid;
-    }
     boolean hasCycle(Node<T> first) {
-        if(first == null)
+        if (first == null)
             return false;
         Node slow, fast;
         slow = fast = first;
-        while(true) {
+        while (true) {
             slow = slow.next;
-            if(fast.next != null)
+            if (fast.next != null)
                 fast = fast.next.next;
             else
                 return false;
-            if(slow == null || fast == null)
+            if (slow == null || fast == null)
                 return false;
-            if(slow == fast)
+            if (slow == fast)
                 return true;
         }
     }
