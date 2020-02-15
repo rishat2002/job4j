d3144a90e22d88d05856bf2b87d3e481c3c453bf
rish <gabdulzhaparov.rischat@yandex.ru>
2020-02-16 00:02:38 +0300

    1. Использую класса Calculator.

---
+++ chapter_003/src/main/java/ru/job4j/calculator/Calculate.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+public class Calculate {
+    public double plus(double firstValue,double secondValue) {
+        return firstValue+secondValue;
+    }
+    public double minus(double firstValue,double secondValue) {
+        return firstValue-secondValue;
+    }
+    public double multipluer(double firstValue,double secondValue) {
+        return firstValue*secondValue;
+    }
+    public double division(double firstValue,double secondValue) {
+        return firstValue/secondValue;
+    }
+}
