ca7034d3d1240d43afdc58ee6cae4c116237ec01
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-03-15 00:59:27 +0300

    1. Хранилище продуктов[#216797]

--- chapter_003/src/main/java/ru/job4j/FoodKeeper/Bread.java
+++ chapter_003/src/main/java/ru/job4j/FoodKeeper/Bread.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 public class Bread extends Food {
     private String type;
-    public Bread(String name, Calendar expaireDate, Calendar createDate, Calendar nowDate, double price, double discount,String type) {
+    public Bread(String name, Calendar expaireDate, Calendar createDate, Calendar nowDate, double price, double discount, String type) {
         super(name, expaireDate, createDate, nowDate, price, discount);
         this.type=type;
     }
--- chapter_003/src/main/java/ru/job4j/FoodKeeper/Trash.java
+++ chapter_003/src/main/java/ru/job4j/FoodKeeper/Trash.java
@@ -3,9 +3,24 @@ package ru.job4j.FoodKeeper;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Trash {
+public class Trash implements Store {
     private List<Food> foods=new ArrayList<>();
     public List<Food> getFoods() {
         return foods;
+    }
+    public double procent(Food food) {
+        long i1=food.getNowDate().getTimeInMillis()-food.getCreateDate().getTimeInMillis();
+        long i2=food.getExpaireDate().getTimeInMillis()-food.getCreateDate().getTimeInMillis();
+        double procent = ((double)i1/(double)i2);
+        return procent;
+    }
+    @Override
+    public boolean accept(Food food) {
+        boolean n=false;
+        if (this.procent(food)>=1.0) {
+            foods.add(food);
+            return n=true;
+        }
+        return n;
     }
 }
