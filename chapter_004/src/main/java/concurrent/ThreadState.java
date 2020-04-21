6c15d25921938972c7690fc5bacf445f2c93b797
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-04-21 19:12:22 +0300

    1.1. Состояние нити.[#273853]

---
+++ chapter_004/src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,20 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED ) {
+
+        }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        System.out.println("Job over");
+    }
+}
