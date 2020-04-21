64aa0fe9ff5fcc19544582662acf080730de4d4c
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-04-21 19:13:38 +0300

    1.2. Режим ожидания.[#273854]

---
+++ chapter_004/src/main/java/concurrent/ThreadSleep.java
@@ -0,0 +1,13 @@
+package concurrent;
+
+public class ThreadSleep {
+    public static void main(String[] args) {
+        for (int index=0;index<=100;index++) {
+        try {
+            Thread.currentThread().sleep(1000);
+            System.out.println("\rLoading : " + index  + "%");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }}
+}
